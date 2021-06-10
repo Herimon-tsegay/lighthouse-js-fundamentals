85b5aab2c6805136b361a2603ef0a3c2f7f43099
Herimon <herimon.tsegay@gmail.com>
2021-06-10 22:05:26 +0000

    packing list Array

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+
+const packingList = ["bowls","plates","pots","pans","eating Utensils ","glasses","cups","cooking utensils"];
+
+console.log("kitchen stuff to pack:");
+for(let i=0;i< packingList.length;i++){
+  console.log(packingList[i]);
+}
