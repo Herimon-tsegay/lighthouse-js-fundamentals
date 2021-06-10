f7013013758a94b68ed58a1b3e6eb92f4ca604ac
Herimon <herimon.tsegay@gmail.com>
2021-06-10 05:13:43 +0000

    checking Odd numbers

---
+++ odd.js
@@ -0,0 +1,19 @@
+/*
+const isEven = function(num){
+  return num %2 === 0;
+
+}
+const  tenIsEven =isEven(10);
+const  elevenIsEven =isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+*/
+
+const isOdd= function(num){
+  return num %2 !== 0;
+
+}
+
+console.log("3  is Odd: " + isOdd(3));
+console.log("8 is Odd: " + isOdd(8));
